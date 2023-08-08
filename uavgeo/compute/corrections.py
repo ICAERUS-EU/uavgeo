c37f2bbee2e9eec7d4f8fd9468f658ce280a5ecc
jurriandoornbos <59558756+jurriandoornbos@users.noreply.github.com>
2023-08-08 12:42:07 +0200

    Update corrections.py

--- uavgeo/compute/corrections.py
+++ uavgeo/compute/corrections.py
@@ -295,3 +295,33 @@ def scale_to_01(arr,max=255):
     """
 
     return arr.astype(np.float16)/max
+
+def scale_band_to_min_max(band, min,max, clip =True):
+    """
+    Scale a band array to a specified minimum and maximum value range.
+
+    This function scales a given band array (`band`) to a specified minimum and maximum value range
+    (`min_value` to `max_value`). Optionally, the scaled values can be clipped to ensure they fall
+    within the specified range.
+
+    Args:
+        band (np.ndarray): The input band array to be scaled.
+        min_value (float): The desired minimum value of the scaled array.
+        max_value (float): The desired maximum value of the scaled array.
+        clip (bool, optional): If True, the scaled values will be clipped to the specified range.
+            Default is True.
+
+    Returns:
+        np.ndarray: A scaled band array with values transformed to fit within the specified
+        minimum and maximum value range.
+
+    Note:
+        The input array `band` is not modified. The function returns a new scaled array.
+    """
+
+    if clip:
+        band = band.clip(min = min, max=max)
+        
+    band = band.astype(float)
+    band = (band-min) * (255/(max-min))
+    return band.astype(np.uint8)
