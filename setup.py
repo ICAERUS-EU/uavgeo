d354cc1e94ee06b7b94de9e14f0a440a19cac947
jurriandoornbos <59558756+jurriandoornbos@users.noreply.github.com>
2023-08-08 12:51:12 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding = 'utf-8') as fh:
 
 setuptools.setup(
     name="uavgeo",
-    version="0.0.rev0",
+    version="0.1.0",
     author="Jurrian Doornbos",
     author_email="jurriandoornbos@gmail.com",
     description="UAV image processing library",
