a882eb3d78513a5fb8b98ff593c55b3a9971c09d
jurriandoornbos <59558756+jurriandoornbos@users.noreply.github.com>
2023-08-30 10:14:16 +0200

    Update __init__.py

--- uavgeo/__init__.py
+++ uavgeo/__init__.py
@@ -4,6 +4,6 @@ uavgeo - The ⛰️ UAV data processing library
 
 from importlib.metadata import version
 
-from uavgeo import compute, load, datapipes
+from uavgeo import compute, load#, datapipes
 
 __version__ = version("uavgeo")  # e.g. 0.1.2.dev3+g0ab3cd78
